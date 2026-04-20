c0fbe256aab382797b3071ed0ee40300041048dc
Krishnagopalan-S <askrishnagopalan@gmail.com>
2026-04-21 04:57:46 +0530

    Update and rename Facebook_logindop.java to Facebook_logindop_business.java

--- Facebook_logindop_business.java
+++ Facebook_logindop_business.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Facebook_logindop {
+public class Facebook_logindop_Business {
 	public static void main(String[] args) throws Throwable {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
